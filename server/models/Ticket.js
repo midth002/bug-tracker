d8e856ec06bdc0f298004d42b37bcb520f93253d
Andrew Midthun <midth002@gmail.com>
2022-05-19 08:32:58 -0500

    Finished User schema and completed Ticket schema.

---
+++ server/models/Ticket.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+
+const ticketSchema = new Schema (
+    {
+        title: {
+            type: String, 
+            required: true, 
+            trim: true,
+            minlength: 1, 
+            maxlength: 280
+        },
+        description: {
+            type: String, 
+            maxlength: 500 
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+        }, 
+        submitter: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        priority: {
+            type: String,
+            required: true
+        }, 
+        type: {
+           type: String,
+           required: true 
+        }  
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const Ticket = model('ticket', ticketSchema);
+
+module.exports = Ticket;
+
--- server/models/User.js
+++ server/models/User.js
@@ -13,6 +13,27 @@ const userSchema = new Schema (
             required: true,
             unique: true,
             match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
-        }
+        },
+        role: {
+            type: Schema.Types.ObjectId, 
+            ref: 'role'
+        },
+        tickets : [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'ticket',
+            },
+        ],
+        
+    }, 
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
     }
-)
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;

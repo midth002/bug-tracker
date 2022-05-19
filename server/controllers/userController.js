8d08884e7f91e78962e6b995bb2989ef91787612
Andrew Midthun <midth002@gmail.com>
2022-05-19 08:55:29 -0500

    Created getUsers function.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,5 +5,10 @@ module.exports = {
         User.create(req.body)
         .then((user) => res.json(user))
         .catch((err) => res.status(500).json(err))
+    }, 
+    getUsers(req, res) {
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
     }
 }
--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const {
-    createUser
+    createUser,
+    getUsers
 } = require('../../controllers/userController');
 
 
 
-router.route('/').post(createUser);
+router.route('/').get(getUsers).post(createUser);
 
 module.exports = router;

42e2b4e097fb42990bbe75b710f4970da41e08b5
Andrew Midthun <midth002@gmail.com>
2022-06-13 14:14:32 -0500

    Styling the projects page.

--- client/src/pages/projects/Projects.js
+++ client/src/pages/projects/Projects.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+
+import React from 'react';
+import ProjectTable from '../../components/projectTable/ProjectTable';
+
+import './projects.scss'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="container">
+        <div className='navbar'>
+            <Navbar />
+        </div>
+        <Sidebar />
+        <div className="projects">
+            <div className="project-header">
+            <h4>Projects</h4>
+                    <div className='create-new-project-div'>
+                        <button>Create a Project</button>
+                    </div>
+                    
+            </div>
+        <ProjectTable />
+        </div>
+    </div>
   )
 }
 

918e3a12671b76a3554efa0c4021ae9d39720cbe
Andrew Midthun <midth002@gmail.com>
2022-06-14 11:27:53 -0500

    working on login functionality.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,3 +13,15 @@ mutation createUser($username: String!, $email: String!, $role: String!, $passwo
   }
 }
 `;
+
+export const LOGIN_USER = gql`
+mutation login($username: String!, $password: String!) {
+  login(username: $username, password: $password) {
+  token
+  user {
+      _id
+      username
+  }
+  }
+}
+`;

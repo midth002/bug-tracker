54ea06143d9397d5655de2b628b8759f59b2bb61
Andrew Midthun <midth002@gmail.com>
2022-07-07 13:14:33 -0500

    save changes

---
+++ client/src/pages/projects/SingleProject.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Stepper from '../../components/stepper/Stepper';
+import ProgressBar from '../../components/progressBar/ProgressBar';
+import TicketForm from '../../components/ticketForm/TicketForm';
+import Loading from '../../components/loading/Loading';
+import CommentForm from '../../components/commentForm/CommentForm'
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ONE_TICKET } from '../../utils/queries';
+import './singleticket.scss';
+
+
+const SingleTicket = () => {
+
+    const params = window.location.href;
+    const paramArray = params.split('/');
+    const ticketNum = paramArray[5]
+
+
+        const { loading: ticketLoading, data: ticketData, error } = useQuery(QUERY_ONE_TICKET, {
+            variables: {
+                ticketId: ticketNum}
+        })
+
+        if (ticketLoading) return <Loading />;
+        if (!ticketData) return <p>Not Found</p>;
+
+      const getTicketInformation = async () => {
+        console.log(ticketData.getOneTicket.priority)
+      }
+
+    getTicketInformation();
+
+
+    return (
+        <div className="container">
+        <div className='navbar'>
+            <Navbar />
+        </div>
+        <Sidebar />
+        <div className="ticket">
+            <div className="ticket-header">
+                <h4>Ticket # {ticketData.getOneTicket._id}</h4>
+            </div>
+                <div className='stepper'>
+                    <ProgressBar 
+                    status={ticketData.getOneTicket.status}
+                    ticketId={ticketData.getOneTicket._id}
+                    />
+                </div>
+                <div className='ticketForm'>
+                    <TicketForm 
+                    priority={ticketData.getOneTicket.priority} 
+                    description={ ticketData.getOneTicket.description} 
+                    type={ticketData.getOneTicket.type}
+                    created={ticketData.getOneTicket.createdAt}
+                    submitter={ticketData.getOneTicket.submitter[0]._id}
+                    ticketId={ticketData.getOneTicket._id}
+                    />
+                </div>
+                <div className='comment-section'>
+                <CommentForm />
+                </div>
+            </div>
+        
+        </div>
+      )
+}
+
+export default SingleTicket;

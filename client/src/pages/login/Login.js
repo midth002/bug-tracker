4a70dec85c584f4b612d2c2997bd3de4c552bcc8
Andrew Midthun <midth002@gmail.com>
2022-07-07 15:52:16 -0500

    styled login page.

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import './login.scss';
-
-
 import Auth from '../../utils/auth';
+import  { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBug } from '@fortawesome/free-solid-svg-icons'
 
 const Login = () => {
     const [formState, setFormState] = useState({ 
@@ -42,45 +46,80 @@ const Login = () => {
       });
     };
     return (
-       <>
+      <div className='first-container'>
+       
+      <Box className="first-box"
+      sx={{
+        position: 'fixed',
+        display: 'flex',
+        flexDirection: { xs: 'column', md: 'row' },
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgcolor: 'background.paper',
+        overflow: 'hidden',
+        fontWeight: 'bold',
+        width: 500,
+        height: 400
+      }}
+    >
+    
                 {data ? (
                  
                    <p>Success</p>
                 
                   
                 ) : (
-                  <form onSubmit={handleFormSubmit} className="login-form">
-                  <span className='login-form-title p-b-26 text-primary'>Login</span>
                  
-                  <div className="validate-input form-input-wrap">
-                    <input
-                      className="form-input rounded-left"
+                 
+                  <form onSubmit={handleFormSubmit} className="login-form">
+                  <FontAwesomeIcon icon={faBug} />
+                  <div className='login-form-title p-b-26 text-primary'>Login to BugTracker</div>
+                 
+                 
+                    <TextField
+                      id="standard-basic" label="username" variant="standard"
                       name="username"
                       type="text"
                       value={formState.username}
                       onChange={handleChange}
-                      required
-                    /><label>Username</label>
-                    </div>
-                    <div className="validate-input form-input-wrap">
-                    <input
-                      className="form-input"
+                      sx={{
+                        width: '70%',
+                        m: 1
+                        }}
                      
+                    />
+                    
+                    <TextField
+                      className="form-input"
+                      id="standard-basic" label="password" variant="standard"
                       name="password"
                       type="password"
                       value={formState.password}
                       onChange={handleChange}
-                      required
-                    /><label>Password</label>
-                    </div>
+                      sx={{
+                        width: '70%',
+                        m: 1
+                        }}
+          
+                    />
+                 
                       {error && (
                   <div className="login-error">
                     {error.message}
                   </div>
                 )}
-                    <div className="login-btn-form">
-                   <button>Login</button>
-                    </div>
+                   
+                   <Button 
+                   type="submit" 
+                   variant="contained" 
+                   color="success"
+                   sx={{
+                    width: '70%',
+                    m: 2,
+                   }}
+                   >Login
+                   </Button>
+                
                   
                   <div>
                   <p className="sign-up-link">Don't have an account? <Link to="/signup">Signup instead</Link></p>
@@ -89,7 +128,8 @@ const Login = () => {
                   
                 )}
               
-                </>
+                </Box>
+                </div>
       );
     };
     export default Login;
